c6c2843953bd08f6c967317e16a4dc5a348e5112
m2492565210 <2492565210@qq.com>
2017-08-09 22:36:01 +0800

    rpc modify

--- src/main/java/com/cnblogs/m2492565210/rpc/RPCClient.java
+++ src/main/java/com/cnblogs/m2492565210/rpc/RPCClient.java
@@ -8,54 +8,64 @@ import com.rabbitmq.client.AMQP.BasicProperties;
 
 public class RPCClient {
 
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
+
     private Connection connection;
     private Channel channel;
-    private String requestQueueName = "rpc_queue";
     private String replyQueueName;
     private QueueingConsumer consumer;
 
     public RPCClient() throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        // 设置MabbitMQ所在主机ip或者主机名
+        factory.setHost("127.0.0.1");
+        // 创建一个连接
         connection = factory.newConnection();
+        // 创建一个频道
         channel = connection.createChannel();
 
+        //声明队列
+        channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+
+        //为每一个客户端获取一个随机的回调队列
         replyQueueName = channel.queueDeclare().getQueue();
+        //为每一个客户端创建一个消费者（用于监听回调队列，获取结果）
         consumer = new QueueingConsumer(channel);
+        //消费者与队列关联
         channel.basicConsume(replyQueueName, true, consumer);
     }
 
-    public static void main(String[] args) {
-        Connection connection = null;
-        Channel channel = null;
+    /**
+     * 获取斐波列其数列的值
+     *
+     * @param message
+     * @return
+     * @throws Exception
+     */
+    public String call(String message) throws Exception{
+        String response = null;
+        String corrId = java.util.UUID.randomUUID().toString();
 
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            connection = factory.newConnection();
-            channel = connection.createChannel();
+        //设置replyTo和correlationId属性值
+        BasicProperties props = new BasicProperties.Builder().correlationId(corrId).replyTo(replyQueueName).build();
 
-            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
-            channel.basicQos(1);
+        //发送消息到rpc_queue队列
+        channel.basicPublish("", RPC_QUEUE_NAME, props, message.getBytes());
 
-            QueueingConsumer consumer = new QueueingConsumer(channel);
-            channel.basicConsume(RPC_QUEUE_NAME, false, consumer);
-
-            System.out.println("RPCServer [x] Awaiting RPC requests");
-
-            while (true) {
-                String response = null;
-
-                QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-
-                BasicProperties props = delivery.getProperties();
-                BasicProperties replyProps =
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody(),"UTF-8");
+                break;
             }
-        } catch (Exception e) {
-
-        } finally {
-
         }
 
+        return response;
+    }
+
+    public static void main(String[] args) throws Exception {
+        RPCClient fibonacciRpc = new RPCClient();
+        String result = fibonacciRpc.call("4");
+        System.out.println( "fib(4) is " + result);
     }
 }
